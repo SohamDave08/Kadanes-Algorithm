7b7fee998c42ba9726d501d4877056abc5413c22
Soham Dave <dave.soham2000@gmail.com>
2020-12-25 03:44:57 +0530

    Minor changes in code

--- code.cpp
+++ code.cpp
@@ -3,13 +3,16 @@ using namespace std;
 
 int KadaneAlgo(int A[], int n)
 {
-    int maxCurrent = A[0];
+    int currSum=0;
     int maxGlobal = INT_MIN;
 
-    for(int i=1;i<n;i++)
+    for(int i=0;i<n;i++)
     {
-        maxCurrent = max(maxCurrent+A[i], A[i]);
-        maxGlobal = max(maxGlobal, maxCurrent);
+        currSum += A[i];
+        maxGlobal = max(maxGlobal, currSum);
+
+        if(currSum<0)
+            currSum=0;
     }
 
     return maxGlobal;
@@ -24,6 +27,6 @@ int main()
         cin>>arr[i];
 
     cout<<KadaneAlgo(arr,n);
-    
+
     return 0;
 }
